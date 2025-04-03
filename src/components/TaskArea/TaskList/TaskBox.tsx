10f8f828856cd99974ab226c1812c64bd9877cd1
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-04-03 09:30:53 -0400

    Fix bug that sent stale checked state to DB [#17]

--- src/components/TaskArea/TaskList/TaskBox.tsx
+++ src/components/TaskArea/TaskList/TaskBox.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import usePostPutPatchDelete from '../../../hooks/usePostPutPatchDelete';
 import { PostPutPatchTaskResponse, TaskData } from '../../../types';
 import DeleteTask from '../AddDeleteTask/DeleteTask';
@@ -13,15 +13,22 @@ const TaskBox: React.FC<{ task: TaskData }> = ({ task }) => {
     PostPutPatchTaskResponse
   >('tasks', 'PATCH');
 
-  const handleChange = (e: React.ChangeEvent) => {
-    e.preventDefault();
-    setComplete(!complete); // Flip the local completion state
-    sendRequest({ id: task.id, task_complete: complete }); // Trigger the request
+  useEffect(() => {
+    if (messageVisible) {
+      const timer = setTimeout(() => setMessageVisible(false), 2500); // Hide the message after 2.5s
+      return () => clearTimeout(timer); // Cleanup in case the component unmounts early
+    }
+  }, [messageVisible]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = e.target.checked; // Capture the checked state
+    setComplete(checked); // Set the component state (asynchronous)
+    sendRequest({ id: task.id, task_complete: checked }); // Trigger the request using the checked state
     setMessageVisible(true); // Show the response message
-    const timer = setTimeout(() => setMessageVisible(false), 2500); // Hide the message after 2.5s
-    return () => clearTimeout(timer); // Cleanup in case the component unmounts early
   };
 
+  // Temporarily hide the component as soon as the user deletes the task. The component will be
+  // fully removed the next time another request is made to the database.
   if (isDeleted) return <></>;
 
   return (
