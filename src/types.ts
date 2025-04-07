56fa68dccaef5713822c50378d29537043c8b6ef
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-04-07 10:08:19 -0400

    Add signup page [#19]

--- src/types.ts
+++ src/types.ts
@@ -22,3 +22,16 @@ export type PostTaskRequired = {
   task_type: TaskType;
   due_date: Date;
 };
+
+export type UserInfo = {
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+};
+
+export type UserSignupUpdateResponse = {
+  message: string;
+  user: UserInfo & { id: number };
+};
