2343f27d1b4a3de0bc685c13342118290d026791
Joshua Peters <Joshua.Peters@colorado.edu>
2025-04-21 16:34:20 -0600

    Added Context for tasks

--- src/pages/TaskPage/TaskPage.tsx
+++ src/pages/TaskPage/TaskPage.tsx
@@ -1,7 +1,7 @@
 import { createContext, Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthService from '../../utils/Auth';
-import { AuthUserData, BalanceData } from '../../types';
+import { AuthUserData, BalanceData, TaskData } from '../../types';
 import GameCanvas from '../../components/GameCanvas/GameCanvas';
 import TaskArea from '../../components/TaskArea/TaskArea';
 import styles from './TaskPage.module.css';
@@ -15,11 +15,28 @@ interface BalanceContextType {
   setUserBalance: Dispatch<SetStateAction<number | null>>;
 }
 
+interface TaskContextType {
+  longTasks: TaskData[] | null;
+  shortTasks: TaskData[] | null;
+  dailyTasks: TaskData[] | null;
+  setLongTasks: Dispatch<SetStateAction<TaskData[] | null>>;
+  setShortTasks: Dispatch<SetStateAction<TaskData[] | null>>;
+  setDailyTasks: Dispatch<SetStateAction<TaskData[] | null>>;
+}
+
 // lets us pass user data to all child components
 export const UserContext = createContext<AuthUserData | null>(null);
+export const TaskContext = createContext<TaskContextType>({
+  longTasks: null,
+  shortTasks: null,
+  dailyTasks: null,
+  setLongTasks: () => { },
+  setShortTasks: () => { },
+  setDailyTasks: () => { }
+});
 export const BalanceContext = createContext<BalanceContextType>({
   userBalance: null,
-  setUserBalance: () => {},
+  setUserBalance: () => { },
 });
 
 const TaskPage = () => {
@@ -28,6 +45,9 @@ const TaskPage = () => {
 
   const [user, setUser] = useState<AuthUserData | null>(null);
   const [userBalance, setUserBalance] = useState<number | null>(0);
+  const [longTasks, setLongTasks] = useState<TaskData[] | null>(null)
+  const [shortTasks, setShortTasks] = useState<TaskData[] | null>(null)
+  const [dailyTasks, setDailyTasks] = useState<TaskData[] | null>(null)
 
   // If the user is not logged in, redirect to the login page
   useEffect(() => {
@@ -57,21 +77,25 @@ const TaskPage = () => {
 
   return (
     <UserContext.Provider value={user}>
-      <BalanceContext.Provider value={{ userBalance, setUserBalance }}>
-        <Container fluid>
-    <Row className="g-0 container">
-      <Col lg={10} md={9} sm={12} className={styles.gameCol}>
-        <div className={styles.gameWrapper}>
-          <GameCanvas />
-        </div>
-      </Col>
-      <Col lg={2} md={3} sm={12} className={styles.taskCol}>
-        <TaskArea />
-      </Col>
-    </Row>
-  </Container>
+      <TaskContext.Provider value={{ longTasks, shortTasks, dailyTasks, 
+                                  setLongTasks, setShortTasks, setDailyTasks }}>
+        <BalanceContext.Provider value={{ userBalance, setUserBalance }}>
 
-      </BalanceContext.Provider>
+          <Container fluid>
+            <Row className="g-0 container">
+              <Col lg={10} md={9} sm={12} className={styles.gameCol}>
+                <div className={styles.gameWrapper}>
+                  <GameCanvas />
+                </div>
+              </Col>
+              <Col lg={2} md={3} sm={12} className={styles.taskCol}>
+                <TaskArea />
+              </Col>
+            </Row>
+          </Container>
+
+        </BalanceContext.Provider>
+      </TaskContext.Provider>
     </UserContext.Provider>
   );
 };
